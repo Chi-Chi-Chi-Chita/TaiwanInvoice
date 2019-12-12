3eb43208e338111b218c343cf9f2a0862f4564e5
Chi-Chi-Chi-Chita <chita0901@gmail.com>
2019-12-12 17:20:32 +0800

    顧客資料建檔頁 JS 的 API 改地址 & 增加統編檢驗 & 統編重覆跳紅字加在 CSS 樣式上

--- js/buyerAdd.js
+++ js/buyerAdd.js
@@ -1,5 +1,52 @@
 var data = {};
+var buyerUniformNumData = {};
 
+//檢查統編
+let buyerUniformNumInput = document.querySelector('#buyerUniformNum');
+
+buyerUniformNumInput.addEventListener('keyup', function (e) {
+    e.preventDefault();
+    let buyerUniformNumValue = buyerUniformNumInput.value
+
+    buyerUniformNumData = { "UniformNumbers": buyerUniformNumValue }
+
+    let buyerInfo = document.querySelector('#buyerInfo');
+    let buyerCreateBtn = document.querySelector('#buyerCreateBtn')
+
+    if (buyerUniformNumValue.length == 8) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                // console.log(this.responseText);
+                if (this.status == 200) {
+                    if (this.responseText == 'true') {
+                        buyerInfo.textContent = "這個統編註冊過了";
+                        buyerCreateBtn.disabled = true;
+                    } else {
+                        buyerInfo.textContent = "";
+                        buyerCreateBtn.disabled = false;
+                    }
+                } else {
+                    alert("伺服器富奸了！") //伺服器打回來的狀況
+                }
+            }
+        });
+
+        xhr.open("POST", "http://invoice.rocket-coding.com/InvClientInfoes/CheckCliUn");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(JSON.stringify(buyerUniformNumData))
+    } else {
+        buyerInfo.textContent = '請輸入8個字元的統編'
+        buyerCreateBtn.disabled = true;
+    }
+
+
+
+
+})
 
 // 對表單的 submit 做監聽
 let form = document.querySelector('#buyerAdd')
@@ -24,7 +71,7 @@ form.addEventListener('submit', function (e) {
         "ContactPhone": buyerContactMobile,
         "ContactEmail": buyerContactEmail,
     }
-    console.log(data);
+    // console.log(data);
 
     var xhr = new XMLHttpRequest();
 
@@ -32,7 +79,7 @@ form.addEventListener('submit', function (e) {
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-            console.log(this.responseText);
+            // console.log(this.responseText);
             if (this.status == 200) {
                 if (this.responseText == 'true') {
                     alert('買受人建檔成功')
@@ -47,7 +94,7 @@ form.addEventListener('submit', function (e) {
         }
     })
 
-    xhr.open("POST", "http://invoice.rocket-coding.com/InvClientInfoes/CreadeClient");
+    xhr.open("POST", "http://invoice.rocket-coding.com/InvClientInfoes/CreadeCliInfo");
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.send(JSON.stringify(data));
