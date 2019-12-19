47c7dade5fdc769d425da50e2adf644b2c3cc376
Chi-Chi-Chi-Chita <chita0901@gmail.com>
2019-12-19 09:24:28 +0800

    電子發票開立 顧客自動完成 & 資料接到資料庫

--- js/invoiceAdd.js
+++ js/invoiceAdd.js
@@ -28,13 +28,15 @@ function showSellInfo() {
     sellTel.value = sellData[0].CompPhoneNumber;
 }
 
-// 顧客公司名稱的自動完成
+// 顧客公司名稱的自動完成 及 顧客 input 的 DOM
 let buyName = document.querySelector('#buyName');
 let dataList = document.querySelector('#json-datalist')
 
 let buyTaxNumber = document.querySelector('#buyTaxNumber');
 let buyAdd = document.querySelector('#buyAdd');
 let buyTel = document.querySelector('#buyTel');
+
+let ClientId;
 
 
 buyName.addEventListener('input', function (e) {
@@ -44,16 +46,18 @@ buyName.addEventListener('input', function (e) {
     }
 
     let dataListOption = document.querySelectorAll('#json-datalist option')
-    console.log(dataList);
+    // console.log(dataList);
     if (dataListOption.length !== 0) {
         dataListOption.forEach(function (item) {
             if (item.value == buyName.value) {
-                console.log(item);
+                // console.log(item);
                 let itemObj = JSON.parse(item.dataset.rawdata);
-                console.log(itemObj);
+                // console.log(itemObj);
                 buyTaxNumber.value = itemObj.UniformNumbers;
                 buyAdd.value = itemObj.CompAddress;
                 buyTel.value = itemObj.CompPhoneNumber;
+                ClientId = itemObj.Id;
+                console.log('顧客ID是' + ClientId);
                 return;
             }
         })
@@ -94,18 +98,83 @@ buyName.addEventListener('input', function (e) {
 
 })
 
+//發票的 DOM
+let invoicePeriod = document.querySelector('#invoicePeriod')
+let invoicePeriodList = document.querySelector('#invoicePeriodList')
+let invoiceNumber = document.querySelector('#invoiceNumber');
+let invoiceNumberList = document.querySelector('#invoiceNumberList')
+let invoiceDate = document.querySelector('#invoiceDate');
+let str = ''
+// let invoiceTime = document.querySelector('.invoiceTime'); 把 Time 拿掉
+
+//抓發票字軌 API
+let invoicePeriodData;
+
+let invoicePeriodxhr = new XMLHttpRequest();
+invoicePeriodxhr.withCredentials = true;
+
+invoicePeriodxhr.open("POST", "http://invoice.rocket-coding.com/InvLetters/SelectInvLet", true);
+invoicePeriodxhr.setRequestHeader("Content-Type", "application/json");
+invoicePeriodxhr.send(null);
+
+invoicePeriodxhr.onload = function () {
+    if (invoicePeriodxhr.status !== 200) { return; }
+    invoicePeriodData = JSON.parse(invoicePeriodxhr.responseText);
+    // console.log(invoicePeriodData);
+
+    for (var i = 0; i < invoicePeriodData.length; i++) {
+        // console.log(invoicePeriodData[i]);
+        str += `<option value="${invoicePeriodData[i].Letter}"></option>`
+    }
+    invoicePeriodList.innerHTML = str;
+}
+
+//判斷發票字軌的內容再去抓發票號碼
+let invoicePeriodSelected = {};
+
+invoicePeriod.addEventListener('change', function (e) {
+    e.preventDefault();
+
+    if (this.value == '') {
+        invoicePeriod.innerHTML = '';
+        return;
+    }
+
+    invoicePeriodSelected = {
+        "Letter": invoicePeriod.value
+    }
+
+    var invoiceNumxhr = new XMLHttpRequest();
+    invoiceNumxhr.withCredentials = true;
+
+    invoiceNumxhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            // console.log(this.responseText);
+            if (this.status == 200) {
+                let invoiceNum = JSON.parse(this.responseText);
+                // console.log(invoiceNum[1]);
+                invoiceNum.forEach(function (item) {
+                    var invoiceNumOption = document.createElement('option');
+                    invoiceNumOption.value = item;
+                    invoiceNumberList.appendChild(invoiceNumOption);
+                })
+            }
+        }
+    });
+
+    invoiceNumxhr.open("POST", "http://invoice.rocket-coding.com/InvTables/SelectInvNm");
+    invoiceNumxhr.setRequestHeader("Content-Type", "application/json");
+    invoiceNumxhr.send(JSON.stringify(invoicePeriodSelected));
+})
 
 
 /*大家的輸入*/
-let invoicePeriod = document.querySelector('#invoicePeriod')
-let invoiceNumber = document.querySelector('#invoiceNumber');
-let invoiceDate = document.querySelector('.invoiceDate');
-let invoiceTime = document.querySelector('.invoiceTime');
+let itemName = document.querySelector('.itemName');
 let taxExcluded = document.querySelector('#taxExcluded');
 let taxPrice = document.querySelector('#taxPrice');
 let taxIncluded = document.querySelector('#taxIncluded');
-let itemName = document.querySelector('.itemName');
 
+let submitAry = [];
 /*發票的新增及刪除功能*/
 let btn = document.querySelector('.btnAdd');
 btn.addEventListener('click', addTbody);
@@ -114,16 +183,13 @@ function addTbody() {
     let tr = document.createElement("tr");
     tr.id = "tr_" + new Date().getTime();
     tr.innerHTML = `
-    <td><input type="text" class="form-control itemName" placeholder="品名" aria-label="Username"
-            aria-describedby="basic-addon1"></td>
-    <td><input type="text" class="form-control amount" aria-label="Username"
-            aria-describedby="basic-addon1"></td>
-    <td><input type="text" class="form-control unitPrice"
-            aria-label="Username" aria-describedby="basic-addon1"></td>
+    <td><input type="text" class="form-control itemName" placeholder="品名" aria-label="Username" aria-describedby="basic-addon1"></td>
+    <td><input type="text" class="form-control amount" placeholder="數量" aria-label="Username" aria-describedby="basic-addon1"></td>
+    <td><input type="text" class="form-control unitPrice" aria-label="Username" aria-describedby="basic-addon1"></td>
     <td class="subtotal"></td>
-    <td><button type="button" class="btn btn-danger btnCal">刪除</button></td>`
+    <td><button type="button" class="btn btn-danger" id="btnCal">刪除</button></td>`
     document.getElementById('invoiceform').appendChild(tr);
-    let delBtnAll = document.querySelectorAll('.btnCal');
+    let delBtnAll = document.querySelectorAll('#btnCal');
     delBtnAll.forEach(function (item, i, arry) {
         // console.log(item); //現在元素
         // console.log(i); //現在元素的位置
@@ -135,11 +201,12 @@ function addTbody() {
 }
 
 function del(e) {
-    let trId = e.target.parentNode.parentNode.id;
-    let trDel = document.getElementById(`${trId}`);
-    console.log(trDel);
+    e.target.parentNode.parentNode.remove(); //Btn 往上一層是 td，再往上一層才是 tr
+    // console.log(e.target.parentNode.parentNode);
+    // let trDel = document.getElementById(`${trId}`);
+    // console.log(trDel);
 
-    document.getElementById('invoiceform').removeChild(trDel)
+    // document.getElementById('invoiceform').removeChild(trDel)
 }
 
 /*發票金額的計算*/
@@ -158,7 +225,7 @@ function count(id) {
     let unitPrice = parseInt(document.querySelector(`#${id} input.unitPrice`).value);
 
     let subCount = amount * unitPrice;
-    console.log(subCount);
+    // console.log(subCount);
     let subtotal = document.querySelector(`#${id} td.subtotal`);
     let subtotalAry = document.querySelectorAll('td.subtotal');
 
@@ -171,7 +238,7 @@ function count(id) {
         let temp = parseInt(subtotalAry[i].innerHTML);
         if (!isNaN(temp)) {
             total += temp;
-            console.log(temp);
+            // console.log(temp);
         };
     }
     taxPrice.innerHTML = (total * 0.05).toFixed(0);
@@ -191,13 +258,13 @@ function invoiceContent() {
     /*組字串*/
     var contentString =
         `H,${sellTaxNumber.value},${sellName.value},${sellAdd.value},${sellTel.value}
-M,${invoiceNumber.value},${dateAry[0]}/${dateAry[1]}/${dateAry[2]} ${invoiceTime.value}:00,07,${buyTaxNumber.value},${buyName.value},${buyAdd.value},1,5,${taxExcluded.textContent},${taxPrice.textContent},${taxIncluded.textContent},1
+M,${invoicePeriod.value}${invoiceNumber.value},${dateAry[0]}/${dateAry[1]}/${dateAry[2]} 10:00:00,07,${buyTaxNumber.value},${buyName.value},${buyAdd.value},1,5,${taxExcluded.textContent},${taxPrice.textContent},${taxIncluded.textContent},1
 `
-    console.log(contentString);
+    // console.log(contentString);
 
     let invoiceformAry = document.querySelectorAll('#invoiceform tr');
     for (i = 0; i < invoiceformAry.length; i++) {
-        console.log(invoiceformAry[i].id);
+        // console.log(invoiceformAry[i].id);
         let thisId = invoiceformAry[i].id;
         let thisitemName = document.querySelector(`#${thisId} input.itemName`).value;
         let thisAmount = document.querySelector(`#${thisId} input.amount`).value;
@@ -212,6 +279,9 @@ M,${invoiceNumber.value},${dateAry[0]}/${dateAry[1]}/${dateAry[2]} ${invoiceTime
 
         contentString += `D,${thisitemName},${thisAmount},${thisUnitPrice},${thisSubtotal},\n`
         console.log(contentString);
+
+        submitAry.push({ "Item": thisitemName, "Count": thisAmount, "Price": thisSubtotal })
+        console.log(submitAry);
     }
 
 
@@ -222,8 +292,9 @@ M,${invoiceNumber.value},${dateAry[0]}/${dateAry[1]}/${dateAry[2]} ${invoiceTime
 let outputBtn = document.querySelector('.output');
 outputBtn.addEventListener('click', createCsvFile);
 function createCsvFile() {
-    var fileName = `${invoiceNumber.value}.csv`;//匯出的檔名
+    var fileName = `${invoicePeriod.value}${invoiceNumber.value}.csv`;//匯出的檔名
     var data = invoiceContent();
+    submitXHR();
     var blob = new Blob([data], {
         type: "application/octet-stream"
     });
@@ -233,4 +304,44 @@ function createCsvFile() {
     link.href = href;
     link.download = fileName;
     link.click();
+}
+/*把資料傳回後端*/
+function submitXHR() {
+    var submitData = {}
+
+    submitData = {
+        "ClientId": ClientId,
+        "Letter": invoicePeriod.value,
+        "Num": invoiceNumber.value,
+        "InvDate": invoiceDate.value + " 10:00:00.977",
+        "Total": taxIncluded.textContent,
+        "Service": submitAry,
+    }
+
+    console.log(JSON.stringify(submitData));
+
+    var submitXhr = new XMLHttpRequest();
+    submitXhr.withCredentials = true;
+
+    submitXhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            // console.log(this.responseText);
+            if (this.status == 200) {
+                if (this.responseText == 'true') {
+                    alert('發票開立資料己存檔 及 CSV檔案匯出成功')
+                    window.location = 'invoiceAdd.html'
+                } else {
+                    alert('建檔失敗');
+                }
+            } else {
+                alert('伺服器QQ')
+            }
+        }
+    });
+
+    submitXhr.open("POST", "http://invoice.rocket-coding.com/InvTables/FinishInv");
+    submitXhr.setRequestHeader("Content-Type", "application/json");
+
+    submitXhr.send(JSON.stringify(submitData));
+
 }
